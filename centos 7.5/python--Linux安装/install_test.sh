5990cce84816771de34c14806f0c7861eaed2697
631132427@qq.com <qq33939946>
2019-09-09 23:51:06 +0800

    初稿完毕

---
+++ centos 7.5/python--Linux安装/install_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#运行操作系统要求：CentOS Linux release 7.2.1511 (Core) 
+
+mkdir -p ~/.config/matplotlib/
+cp -rf matplotlibrc.config  	~/.config/matplotlib/matplotlibrc
+echo "add shell, make telnet(shell commond ) can run matplotlibrc lib."
+
+
